6a4d9f9b37e8ad08b71461e1565540054a040338
Ali Savas <152027865+alisavas7@users.noreply.github.com>
2023-12-19 19:57:28 -0500

    Update load.js

--- load.js
+++ load.js
@@ -1,2 +1,19 @@
 const img_src = sessionStorage.getItem('image_source');
-console.log(img_src);
+const img = document.createElement('img');
+img.setAttribute('data-testid', 'value-image-image');
+img.setAttribute('src', img_source);
+img.setAttribute('alt', 'image');
+img.setAttribute('class', 'max-w-full');
+
+const a =document.createElement('a');
+a.setAttribute('data-testid', 'value-image-url-image');
+a.setAttribute('href', img_source);
+a.setAttribute('rel', 'noreferrer');
+a.setAttribute('target', '_blank');
+a.setAttribute('class', 'inline-flex flex-col');
+a.append(img);
+
+const div = document.createElement('div');
+div.setAttribute('data-testid', 'value-image-input');
+div.append(a);
+document.querySelector("[data-testid=image-file-input]").append(div);
