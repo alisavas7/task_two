ce5990d6e8c1269b6bd6ecf06483c282f2444574
Ali Savas <152027865+alisavas7@users.noreply.github.com>
2023-12-20 13:42:31 -0500

    Update run.js

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-import domtoimage from "https://www.jsdelivr.com/package/npm/dom-to-image";
+import domtoimage from "https://github.com/tsayen/dom-to-image/blob/fae625bce0970b3a039671ea7f338d05ecb3d0e8/src/dom-to-image.js";
 
 const run = document.querySelector("[data-testid=input-form]");
 
