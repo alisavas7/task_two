83b17e8837cca09bcfd97d41f74c7add492721bc
Ali Savas <152027865+alisavas7@users.noreply.github.com>
2023-12-29 17:25:46 -0500

    Update run.js

--- run.js
+++ run.js
@@ -156,7 +156,7 @@ function generate_flyer (logo_url, hex_code, punchline, button_text, image_url =
     /* API request */
     const api = 'http://localhost:8080/html-to-image';
     const options = {
-        method: 'POST',
+        method: 'GET',
         mode: 'no-cors',
         headers: {
             'content-type': 'application/json',
@@ -167,8 +167,7 @@ function generate_flyer (logo_url, hex_code, punchline, button_text, image_url =
             'flyer' : flyer_banner
         }
     }
-    const fetched = fetch(api, options);
-    console.log(options.body.flyer);   
+    const fetched = fetch(api, options).then((response) => response.json()).then((json) => console.log(json));;  
 }
 
 function raise_error(message, name) {
