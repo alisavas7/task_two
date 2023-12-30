5e54a1f68df969bf8c8135e495821ec3b5dce89c
Ali Savas <152027865+alisavas7@users.noreply.github.com>
2023-12-30 15:27:30 -0500

    Update run.js

--- run.js
+++ run.js
@@ -159,6 +159,7 @@ async function generate_flyer (logo_url, hex_code, punchline, button_text, image
     const gen_image_inside_style = `.gen-image {width: 100%; height: 100%; border-radius: 25%;}`;
     const fs = `${flyer_style} ${pl_style} ${btn_style} ${btn_text} ${logo_outside_style} ${logo_inside_style} ${gen_image_outside_style} ${gen_image_inside_style}`;
     const flyer_html = `<html><head><style>${fs}</style></head><body>${flyer_banner.innerHTML}</body></html>`;
+    console.log(flyer_html.type);
     /* API request */
     const inputs = {
         'flyer_html' : flyer_html
@@ -174,7 +175,6 @@ async function generate_flyer (logo_url, hex_code, punchline, button_text, image
         },
         body: JSON.stringify(inputs)
     };
-    console.log(options.body);
     fetch(apiUrl, options)
         .then(response => {
             if(!response.ok){
